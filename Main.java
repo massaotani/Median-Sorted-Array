4f7fb0baf605e4135e0c1f36b6c23de219f67fa8
Massao <tani22massao@gmail.com>
2025-06-19 00:19:54 +0900

    format + rearrange

--- Main.java
+++ Main.java
@@ -1,51 +1,64 @@
 import java.util.Scanner;
 
-class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Enter the array (nums1): ");
-        String nums1 = sc.nextLine();
+class Main {
+    public static void main(String[] args) {
+        String nums1;
+        String nums2;
 
-        System.out.println("Now, enter the array (nums2): ");
-        String nums2 = sc.nextLine();
-
-        sc.close();
-
-        String[] aux = NewStringFormat(nums1);
-        String[] _aux = NewStringFormat(nums2);
-
-        // System.out.println("Nums1: " + aux + "/ Nums2: " + _aux);
-
-        int[] arr = ArrayRearranged(aux);
-        int[] _arr = ArrayRearranged(_aux);
-
-        System.out.println(arr);
-
-        return;
-    }
-
-    public static int[] ArrayRearranged(String[] nums){
-        int[] arr = new int[nums.length];
-
-        for(int i = 0; i < arr.length; i++){
-            try{
-                arr[i] = Integer.parseInt(nums[i]);
-            }catch(NumberFormatException e){
-                System.err.println("Error: " + nums[i] + "is not a valid Integer value.");
-            }
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the array (nums1): ");
+            nums1 = sc.nextLine();
+            System.out.println("Now, enter the array (nums2): ");
+            nums2 = sc.nextLine();
         }
 
-        return arr;
+        int[] arr = ArrayRearrange(nums1, nums2);
+        double result = CalculateMedianValue(arr);
+
+        System.out.println(result);
     }
 
-    public static String[] NewStringFormat(String num){
-        String newString = num.replace("[|]", "").toString();
-        System.out.println(newString);
-        String[] stringNumbers = newString.split(",");
+    public static int[] ArrayRearrange(String nums1, String nums2) {
+        int[] aux = NewStringFormat(nums1);
+        int[] _aux = NewStringFormat(nums2);
 
-        System.out.println(stringNumbers);
+        int[] newArr = new int[aux.length + _aux.length];
 
-        return stringNumbers;
+        // Merge the two arrays into one.
+        System.arraycopy(aux, 0, newArr, 0, aux.length);
+        System.arraycopy(_aux, 0, newArr, aux.length, _aux.length);
+
+        newArr = MergeSort(newArr);
+
+        return newArr;
+    }
+
+    public static int[] MergeSort(int[] arr){
+        int[] res = new int[arr.length];
+
+        return res;
+    }
+
+    public static double CalculateMedianValue(int[] arr){
+        double median = 0.0;
+
+        return median;
+    }
+
+    public static int[] NewStringFormat(String num) {
+        String[] stringNumbers = num.split(",");
+        int[] intArray = new int[stringNumbers.length];
+
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = Integer.parseInt(stringNumbers[i].trim());
+        }
+        
+        return intArray;
+    }
+
+    public static void PrintArray(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
